de8b6cf0dc6cd93f1bfe08c6bc12f6414fcd34d9
tussi5969 <sakura.0304.0226@gmail.com>
2019-09-17 17:07:40 +0900

    timezone to jp

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ module ChatSpace
       g.test_framework false
       config.i18n.default_locale = :ja
     end
+
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
